89459a30344986bf6f92ffd8616d3d01154168c2
Rodrigo <rodrigo@delivoro.com.br>
2021-07-03 19:32:12 -0300

    Ajustes para mostrar como injetar um mapper

--- src/main/java/training/actionbeans/BaseActionBean.java
+++ src/main/java/training/actionbeans/BaseActionBean.java
@@ -5,14 +5,26 @@ import lombok.Getter;
 import lombok.Setter;
 import net.sourceforge.stripes.action.ActionBean;
 import net.sourceforge.stripes.action.ActionBeanContext;
+import net.sourceforge.stripes.validation.SimpleError;
 
 /**
  * Classe base de todas as ActionBeans, que contém o código compartilhado entre elas.
- * 
+ *
  * @author Davi Bezerra
  */
 public abstract class BaseActionBean implements ActionBean {
 
     /** O contexto gerado pelo Stripes. */
     private @Getter @Setter ActionBeanContext context;
+
+    /**
+     * Adiciona uma String com mensagem de erro no contexto do obj ValidationErrors recebido como parametro.
+     *
+     * @param msg       String com a mensagem de erro
+     * @param parameter parametros (opcional)
+     */
+    public void addSimpleErrorMessage(String msg, Object... parameter) {
+        getContext().getValidationErrors().addGlobalError(
+            new SimpleError(msg, parameter));
+    }
 }
--- src/main/java/training/actionbeans/IndexActionBean.java
+++ src/main/java/training/actionbeans/IndexActionBean.java
@@ -5,10 +5,12 @@ import net.sourceforge.stripes.action.DefaultHandler;
 import net.sourceforge.stripes.action.ForwardResolution;
 import net.sourceforge.stripes.action.Resolution;
 import net.sourceforge.stripes.action.UrlBinding;
+import net.sourceforge.stripes.integration.spring.SpringBean;
+import training.mapper.ExampleMapper;
 
 /**
  * Página principal do sistema.
- * 
+ *
  * @author Davi Bezerra
  */
 @UrlBinding("/index.action")
@@ -16,10 +18,18 @@ public class IndexActionBean extends BaseActionBean {
 
     /** View de início do sistema. */
     private final static String VIEW_INDEX = "/WEB-INF/jsp/index.jsp";
-    
+    private final static String ERROR_JSP = "/WEB-INF/jsp/error.jsp";
+
+    @SpringBean
+    private ExampleMapper exampleMapper;
+
     @DefaultHandler
     public Resolution index() {
+        if (exampleMapper == null) {
+            addSimpleErrorMessage("Teste de sanidade do projeto: exampleMapper nao foi injetado na action");
+            return new ForwardResolution(ERROR_JSP);
+        }
         return new ForwardResolution(VIEW_INDEX);
     }
-    
+
 }
