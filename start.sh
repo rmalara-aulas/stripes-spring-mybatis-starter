7739401bb2b9683a9f374d7ca657bb76a8e52673
Victor Marquez <estag2@delivoro.com.br>
2021-07-13 17:28:58 -0300

    Ajuste JSTL

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-${CATALINA_HOME}/bin/shutdown.sh &&  cp build/libs/starter-1.0.0.war ${CATALINA_HOME}/webapps/starter.war && rm -rf ${CATALINA_HOME}/webapps/starter ${CATALINA_HOME}/work/* && ${CATALINA_HOME}/bin/startup.sh && tail -f ${CATALINA_HOME}/logs/*log
+./gradlew assemble && ${CATALINA_HOME}/bin/shutdown.sh &&  cp build/libs/starter-1.0.0.war ${CATALINA_HOME}/webapps/starter.war && rm -rf ${CATALINA_HOME}/webapps/starter ${CATALINA_HOME}/work/* && ${CATALINA_HOME}/bin/startup.sh && tail -f ${CATALINA_HOME}/logs/*log
 
